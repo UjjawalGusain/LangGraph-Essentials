34ac50e680abf6322c1bc30659a9bb3cf7754c3a
Ujjawal Gusain <ujjawalgusain31@gmail.com>
2026-05-15 15:02:27 +0530

    Advanced chatbot with sqlite database added

---
+++ advanced_chatbot/langgraph_db_backend.py
@@ -0,0 +1,62 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from pydantic import BaseModel, Field
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated, Literal
+from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage, BaseMessage, AIMessage
+
+from langgraph.graph.message import add_messages
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
+
+import os
+
+import operator
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-3.1-flash-lite-preview",   
+    google_api_key=os.getenv('GEMINI_API_KEY')
+)
+
+class ChatState(TypedDict):
+    messages: Annotated[list[BaseMessage], add_messages]
+
+def chat_node(state: ChatState):
+    # take user query
+    messages = state['messages']
+
+    # send to llm
+    response = llm.invoke(messages).content
+    structured_response = AIMessage(content=(response[0]['text']))
+
+    # response
+    return {'messages': [structured_response]}
+
+#############################CHECKPOINTER################################
+conn = sqlite3.connect(database="advanced_chatbot/chatbot.db", check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
+
+
+############################# GRAPH #####################################
+graph = StateGraph(ChatState)
+
+graph.add_node("chat_node", chat_node)
+
+graph.add_edge(START, "chat_node")
+graph.add_edge("chat_node", END)
+
+chatbot = graph.compile(checkpointer=checkpointer)
+
+def retreive_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config["configurable"]["thread_id"])
+    
+    return (list(all_threads));
+
+
+# config = { 'configurable': {'thread_id': 1} }
+# response = chatbot.invoke({'messages': [ HumanMessage(content = "What is my name?") ]}, config=config)
+# print(response)
